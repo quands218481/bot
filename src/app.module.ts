4ceaf58c76aac38a762ee6f2b4566ca0cfcbab48
macbook <quan080195@gmail.com>
2023-12-18 10:45:56 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { LarkSuiteModule } from './larksuite/larksuite.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(<string>process.env.MONGOOSE_URL),
+    MongooseModule.forRoot('mongodb+srv://admin:6IKdolkCSHPSSyZh@cluster0.s2gdn.mongodb.net/WealifyTest?retryWrites=true&w=majority'),
     // TelegrafModule.forRootAsync({
     //   useFactory: () => ({
     //     botName: <string>process.env.TELEGRAM_OXANON_BOT_NAME,
