9e06d408d2ce24438003291eef37f7c7cbb0ea9a
macbook <quan080195@gmail.com>
2023-12-18 10:43:11 +0700

    update

--- src/larksuite/larksuite.module.ts
+++ src/larksuite/larksuite.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { LarkSuiteService } from './larksuite.service';
 import { ConfigModule } from '@nestjs/config';
 import { LarkSuiteController } from './larksuite.controller';
+import { LarkSuiteSchema, Larksuite } from './larksuite.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule,
+    MongooseModule.forFeature(
+      [
+        {
+          name: Larksuite.name,
+          schema: LarkSuiteSchema,
+        },])
+  ],
   providers: [LarkSuiteService],
   controllers: [LarkSuiteController],
   exports: [LarkSuiteService],
